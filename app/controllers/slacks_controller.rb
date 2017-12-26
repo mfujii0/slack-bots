ff60f2729cd425249e25cbde6b0f5f82f4dcd162
mfujii <mfujii@aiming-inc.com>
2017-12-26 16:22:01 +0900

    つらいときに挙げる札を挙げる猫

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -7,4 +7,35 @@ class SlacksController < ApplicationController
       response_type: 'in_channel'
     }.to_json, content_type: 'application/json'
   end
+
+  def tsurai
+    text = "`#{params[:user_name]}` さんが `#{params[:text]}時に挙げる札` を挙げました。"
+    json = {
+      text: text,
+      response_type: 'in_channel',
+      attachments: [
+        {
+          image_url: view_context.image_url('/slack/tsurai/images/tsurai.jpg', only_path: false)
+        }
+      ]
+    }.to_json
+
+    # TODO
+    # 画像加工が3秒制限に収まらなければ success だけ先に返すようにする
+    post_json(params[:response_url], json)
+    head :ok
+  end
+
+  private
+
+  def post_json(url, json)
+    uri = URI.parse(url)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    post = Net::HTTP::Post.new(uri.path)
+    post.body = json
+    post["Content-Type"] = "application/json"
+    https.request(post)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :slack, only: :nil do
     post :sudden_death
+    post :tsurai
   end
 end
