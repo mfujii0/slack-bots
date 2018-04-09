093f4dc130b796a96d97950ed3f43a775c82d76c
mfujii <mfujii@aiming-inc.com>
2018-04-09 20:58:46 +0900

    うれしい時に挙げる札 /tsurai -u オプション追加

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -17,8 +17,28 @@ class SlacksController < ApplicationController
   end
 
   def tsurai
-    image = generate_tsurai_image(params[:text])
-    text = "`#{params[:user_name]}` さんが `#{params[:text]}` を挙げました。"
+    options = {}
+    OptionParser.new do |opt|
+      opt.on('-u') { options[:u] = true }
+      opt.parse(params[:text].split)
+    end
+    message = params[:text].gsub(/(^|\s+)\-u($|\s+)/, '')
+
+    original_file = ''
+    default_message = ''
+    font = ''
+    if options[:u]
+      original_file = Rails.root + 'app/assets/images/ureshii.png'
+      default_message = 'うれしい時に挙げる札'
+      font = ENV['SLACK_URESHII_FONT_FILE']
+    else
+      original_file = Rails.root + 'app/assets/images/tsurai.jpg'
+      default_message = 'つらい時に挙げる札'
+      font = ENV['SLACK_TSURAI_FONT_FILE']
+    end
+
+    image = generate_tsurai_image(message, original_file, default_message, font)
+    text = "`#{params[:user_name]}` さんが `#{message}` を挙げました。"
     image_url = request.base_url + '/' + image
     json = {
       text: text,
@@ -33,7 +53,7 @@ class SlacksController < ApplicationController
     post_json(params[:response_url], json)
 
     log = {
-      text: "`#{params[:user_name]}` さんが ##{params[:channel_name]} で `#{params[:text]}` を挙げました。",
+      text: "`#{params[:user_name]}` さんが ##{params[:channel_name]} で `#{message}` を挙げました。",
       channel: %w(directmessage privategroup).include?(params[:channel_name]) ? '#tsurai-private-log' : '#tsurai-log',
       link_names: 1,
       attachments: [
@@ -80,21 +100,20 @@ class SlacksController < ApplicationController
     https.request(post)
   end
 
-  ORIGINAL_FILE = Rails.root + 'app/assets/images/tsurai.jpg'
-  def generate_tsurai_image(message)
-    message = 'つらい時に挙げる札' if message.blank?
+  def generate_tsurai_image(message, original_file, default_message, font)
+    message = default_message if message.blank?
     message_len = message.length
     message = message.chars.join("\n")
     file_name = "#{SecureRandom.hex}.jpg"
     file_path = "public/slack/tsurai/images/#{file_name}"
 
-    image = Magick::Image.read(ORIGINAL_FILE)[0]
+    image = Magick::Image.read(original_file)[0]
     characters = Magick::Image.new(image.columns, image.rows) { self.background_color = 'none' }
 
     draw = Magick::Draw.new # 描画オブジェクト
 
     draw.annotate(characters, 0, 0, 104, 0, message) do
-      self.font = ENV['SLACK_TSURAI_FONT_FILE'] # フォント
+      self.font = font # フォント
       self.fill = 'black' # フォント塗りつぶし色
       self.stroke = 'transparent' # フォント縁取り色
       self.pointsize = [36, 288.0 / message_len].min # フォントサイズ
@@ -108,4 +127,3 @@ class SlacksController < ApplicationController
     "slack/tsurai/images/#{file_name}"
   end
 end
-
