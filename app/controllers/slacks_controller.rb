c6551f45bdec03eb466d861bf1d9fa562fa646dc
mfujii <mfujii@aiming-inc.com>
2019-02-14 10:01:09 +0900

    textがない場合、突然の死が突然の死を迎えるのを修正

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -2,8 +2,9 @@ class SlacksController < ApplicationController
   protect_from_forgery except: [:sudden_death]
 
   def sudden_death
+    text = (params[:text].presence || '突然の死').sudden_death
     render plain: {
-      text: "```#{params[:text].sudden_death}```",
+      text: "```#{text}```",
       response_type: 'in_channel'
     }.to_json, content_type: 'application/json'
   end
