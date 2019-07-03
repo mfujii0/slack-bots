2ac5b274c6597bf4b02c758098c926a819eb65a9
mfujii <mfujii@aiming-inc.com>
2019-07-03 11:15:08 +0900

    Revert "primitive_language 仮実装"
    This reverts commit 65f1543937c136bc9852d32db7d9d4e56d8e1cff.
    
    CLOUD NATURAL LANGUAGE API は日本語の読み方を提供していなかったので

--- app/controllers/primitive_language_controller.rb
+++
@@ -1,8 +0,0 @@
-class PrimitiveLanguageController < ApplicationController
-  def translate
-    render plain: {
-      text: 'オレサマ　オマエ　マルカジリ',
-      response_type: 'in_channel'
-    }.to_json, content_type: 'application/json'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
     post :yoki
     post :xian
   end
-  post :primitive_language, to: 'primitive_language#translate'
 end
